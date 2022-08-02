e6a54a5564461a06ee91c3f0440221089afc8f74
George <gnjuki19@gmail.com>
2022-08-02 23:23:37 +0300

    Added some styling to the button elements

--- src/NotesForm.js
+++ src/NotesForm.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import notes from "./notes";
 
 function NotesForm(){
+
+    const [newNote, setNewNote] = useState({
+        title: "",
+        content: ""
+    })
+
+    function handleChanges(event){
+        const {name, value} = event.target;
+        setNewNote(prevState => {
+            return {
+                ...prevState,
+                [name]: value
+            }
+        })
+    }
+
+    function submitNote(event){
+
+    }
+
+
     return (
         <div>
             <form>
-                <input type="text" name="title" placeholder="Enter your Title..." />
-                <textarea name="content" placeholder="Enter your Content..." />
-                <button>Add Note</button>
+                <input type="text" name="title" value={notes.title} onChange={handleChanges} placeholder="Enter your Title..." />
+                <textarea name="content" value={notes.content} onChange={handleChanges} placeholder="Enter your Content..." />
+                <button onClick={submitNote}>Add Note</button>
             </form>
         </div>
     )
